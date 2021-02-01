905c92bde2ceca29ef31e860e3695afabe9cb278
2auk <ahmada1uk@googlemail.com>
2021-02-01 00:11:52 +0000

    Input method changed (dir scanning)

--- gridcollector.py
+++ gridcollector.py
@@ -6,18 +6,20 @@ from SFED_routines import sfed_kh_3drism, integrate_sfed
 
 class GridCollector():
 
-    def __init__(self, mol, fname, suffix=["H", "C", "G", "U"]):
+    def __init__(self, mol, fname, suffix=["H", "C", "G"]):
         self.mol = mol
         self.fname = fname
         self.suffix = suffix
         self.delta = None
         self.grids = {}
-        self.__U_loaded = False
         self.read()
 
     def read(self):
         DX = Path(self.fname).glob("*.dx")
         files = [p for p in DX if p.is_file()]
+        if not files:
+            raise Exception("No .dx files found")
+
         for dx in files:
             name = dx.stem.split('.')
             if (name[0].endswith("_"+self.suffix[0])):
@@ -27,10 +29,10 @@ class GridCollector():
                 self.grids[self.suffix[1]+name[1]] = Grid(self.fname + "/" + dx.stem + ".dx")
             elif (name[0].endswith("_"+self.suffix[2])):
                 self.grids[self.suffix[2]+name[1]] = Grid(self.fname + "/" + dx.stem + ".dx")
-            elif (name[0].endswith("_"+self.suffix[3])):
-                self.grids[self.suffix[3]+name[1]] = Grid(self.fname + "/" + dx.stem + ".dx")
-            else:
-                print("cant find anything")
+            # elif (name[0].endswith("_"+self.suffix[3])):
+            #     self.grids[self.suffix[3]+name[1]] = Grid(self.fname + "/" + dx.stem + ".dx")
+        if not self.grids:
+            raise Exception("No tagged .dx files found")
 
 if __name__ == "__main__":
     base_path = Path(__file__).parent
--- sfed.py
+++ sfed.py
@@ -3,9 +3,17 @@ from SFED_routines import *
 import numpy as np
 import sys
 import argparse
+from gridcollector import GridCollector
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--ho", help="[INPUT] O Total Correlation Function .dx")
+
+parser.add_argument("-d", "--directory", help=" Directory to be scanned containing dx files", required=True)
+parser.add_argument("-i", "--input", help="Name of input molecule", required=True)
+parser.add_argument("-c", "--closure", help="Closure for SFE functional [KH, HNC or GF]", required=True)
+parser.add_argument("-o", "--output", help = "Output file name", required=True)
+parser.add_argument("-T", "--temperature",  help="Temperature of system (default = 300)", type=float, nargs="?", default=300)
+parser.add_argument("-p", "--density", help="Density of system (default = 0.03342285869 [for water])", type=float, nargs="?", default=3.3422858685000001E-02)
+""" parser.add_argument("--ho", help="[INPUT] O Total Correlation Function .dx")
 parser.add_argument("--co", help="[INPUT] O Direct Correlation Function .dx")
 parser.add_argument("--hh", help="[INPUT] H Total Correlation Function .dx")
 parser.add_argument("--ch", help="[INPUT] H Direct Correlation Function .dx")
@@ -16,8 +24,8 @@ parser.add_argument("--uh", help="[PSEN INPUT] H Potential Energy Grid .dx")
 #parser.add_argument("gh", help="[INPUT, OPTIONAL] H RDF .dx")
 parser.add_argument("-c", "--closure", help="closure for SFE functional [KH, HNC or GF]")
 parser.add_argument("-o", "--output", help = "Output file name")
-parser.add_argument("--temp", help="Temperature of system (default = 300)", type=float, nargs="?", default=300)
-parser.add_argument("--rho", help="Density of system (default = 0.03342285869 [for water])", type=float, nargs="?", default=0.03342285869)
+parser.add_argument("--temperature", help="Temperature of system (default = 300)", type=float, nargs="?", default=300)
+parser.add_argument("--rho", help="Density of system (default = 0.03342285869 [for water])", type=float, nargs="?", default=0.03342285869) """
 args = parser.parse_args()
 
 def epilogue(output_sfed, sample_grid, fname):
@@ -26,20 +34,19 @@ def epilogue(output_sfed, sample_grid, fname):
     print("SFED written to " + fname + ".dx")
 
 if __name__ == "__main__":
-    ho = Grid(args.ho)
-    co = Grid(args.co)
-    hh = Grid(args.hh)
-    ch = Grid(args.ch)
+    data_path = args.directory
+    mol_name = args.input
+    grids = GridCollector(mol_name, data_path)
 
     if args.closure == "KH":
-        output_sfed = sfed_kh_3drism(ho.grid, co.grid, hh.grid, ch.grid, rho=args.rho, T=args.temp)
-        epilogue(output_sfed, ho, args.output)
+        output_sfed = sfed_kh_3drism(grids.grids["HO"].grid, grids.grids["CO"].grid, grids.grids["HH1"].grid, grids.grids["CH1"].grid, rho=args.density, T=args.temperature)
+        epilogue(output_sfed, grids.grids["HO"], args.output)
     elif args.closure == "GF":
-        output_sfed = sfed_gf_3drism(ho.grid, co.grid, hh.grid, ch.grid, rho=args.rho, T=args.temp)
-        epilogue(output_sfed, ho, args.output)
+        output_sfed = sfed_gf_3drism(grids.grids["HO"].grid, grids.grids["CO"].grid, grids.grids["HH1"].grid, grids.grids["CH1"].grid, rho=args.density, T=args.temperature)
+        epilogue(output_sfed, grids.grids["HO"].grid, args.output)
     elif args.closure == "HNC":
-        output_sfed = sfed_hnc_3drism(ho.grid, co.grid, hh.grid, ch.grid, rho=args.rho, T=args.temp)
-        epilogue(output_sfed, ho, args.output)
+        output_sfed = sfed_hnc_3drism(grids.grids["HO"].grid, grids.grids["CO"].grid, grids.grids["HH1"].grid, grids.grids["CH1"].grid, rho=args.density, T=args.temperature)
+        epilogue(output_sfed, grids.grids["HO"].grid, args.output)
     else:
-        print("Closure not implemented.")
+        raise Exception("Unknown closure")
 
