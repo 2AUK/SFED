9385235c84b64fd8775f6107aff62df75497d9cc
2auk <ahmada1uk@googlemail.com>
2021-02-01 15:00:51 +0000

    Added usage instructions to README.md

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ SFED
 Computes 3D solvent free energy distributions around molecules
 
 Requires AMBER .dx files as input
+
+Usage
+==============================
+
+The program scans a folder containing the output .dx files from a 3DRISM Amber calculation.
+
+The main information required is the directory you're scanning, the name of the input file and the closure.
+
+The .dx files require a suffix tag as well for the program to differentiate between the total correlation function, direct correlation function and pair distribution function.
+
+For example, if you want to run a 3DRISM calculaton on 3-methylbutan-1ol, your job command may look like this:
+
+    rism3d.snglpnt --pdb 3methbut1ol.pdb --prmtop 3methbut1ol.prmtop --closure KH --xvv SPC_NaCl.xvv --guv 3methbut1ol_G --huv 3methbut1ol_H --cuv 3methbut1ol_C --uuv 3methbut1ol_U --gf --pc+  > 3methbut1ol.out
+
+The above command takes the pdb, parameter, closure and solvent susceptibility (xvv) as inputs.
+The output file names are given as described after the guv, huv, cuv and uuv flags with the appropriate G, H, C and U tags respectively.
+
+You may then run the sfed.py script as such:
+
+    python sfed.py -d <output directory of .dx files from AMBER> -i 3methbut1ol -o 3methbut1ol_SFED -c KH -p 0.03342285869 -T 300
+
+The last two flags, p and T are the density and temperature respectively. Both are optional, as the default for p is 0.03342285869 A^-3 - the density of water. The temperature is by default at 300K.
--- sfed.py
+++ sfed.py
@@ -13,19 +13,7 @@ parser.add_argument("-c", "--closure", help="Closure for SFE functional [KH, HNC
 parser.add_argument("-o", "--output", help = "Output file name", required=True)
 parser.add_argument("-T", "--temperature",  help="Temperature of system (default = 300)", type=float, nargs="?", default=300)
 parser.add_argument("-p", "--density", help="Density of system (default = 0.03342285869 [for water])", type=float, nargs="?", default=3.3422858685000001E-02)
-""" parser.add_argument("--ho", help="[INPUT] O Total Correlation Function .dx")
-parser.add_argument("--co", help="[INPUT] O Direct Correlation Function .dx")
-parser.add_argument("--hh", help="[INPUT] H Total Correlation Function .dx")
-parser.add_argument("--ch", help="[INPUT] H Direct Correlation Function .dx")
-parser.add_argument("--uo", help="[PSEN INPUT] O Potential Energy Grid .dx")
-parser.add_argument("--uh", help="[PSEN INPUT] H Potential Energy Grid .dx")
-#parser.add_argument("use_gr", help="[OPTION] User g(r) function to compute h(r)[= g(r) - 1], 1 (yes) or 0 (no)", action="store_true")
-#parser.add_argument("go", help="[INPUT, OPTIONAL] O RDF .dx")
-#parser.add_argument("gh", help="[INPUT, OPTIONAL] H RDF .dx")
-parser.add_argument("-c", "--closure", help="closure for SFE functional [KH, HNC or GF]")
-parser.add_argument("-o", "--output", help = "Output file name")
-parser.add_argument("--temperature", help="Temperature of system (default = 300)", type=float, nargs="?", default=300)
-parser.add_argument("--rho", help="Density of system (default = 0.03342285869 [for water])", type=float, nargs="?", default=0.03342285869) """
+#parser.add_argument("-t", "--tags", help="Suffix tags for scanning the correct .dx files (default = [\"H\", \"C\", \"G\"])", nargs="+", default=["H", "C", "G"])
 args = parser.parse_args()
 
 def epilogue(output_sfed, sample_grid, fname):
@@ -36,6 +24,7 @@ def epilogue(output_sfed, sample_grid, fname):
 if __name__ == "__main__":
     data_path = args.directory
     mol_name = args.input
+    #suffixes = args.tags
     grids = GridCollector(mol_name, data_path)
 
     if args.closure == "KH":
@@ -49,4 +38,3 @@ if __name__ == "__main__":
         epilogue(output_sfed, grids.grids["HO"].grid, args.output)
     else:
         raise Exception("Unknown closure")
-
